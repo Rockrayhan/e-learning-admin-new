182f54a926d655df904d98462520d80a0a1c5efb
rockRayhan <khayrulalamdict@gmail.com>
2024-02-10 13:20:53 +0600

    asd

--- resources/views/frontend/productDetails.blade.php
+++ resources/views/frontend/productDetails.blade.php
@@ -428,6 +428,24 @@
                         </div>
                     </div>
                 </div>
+
+            {{--   pro content  --}}
+                <div>
+                    <div class="row">
+                        <div class="col-3">
+                          <div class="nav flex-column nav-pills" id="v-pills-tab" role="tablist" aria-orientation="vertical">
+                            <button class="nav-link active" id="v-pills-home-tab" data-toggle="pill" data-target="#v-pills-home" type="button" role="tab" aria-controls="v-pills-home" aria-selected="true">Home</button>
+                           
+                          </div>
+                        </div>
+                        <div class="col-9">
+                          <div class="tab-content" id="v-pills-tabContent">
+                            <div class="tab-pane fade show active" id="v-pills-home" role="tabpanel" aria-labelledby="v-pills-home-tab">home Content</div>
+                          </div>
+                        </div>
+                      </div>
+
+                </div>
             </div>
         </div>
 
